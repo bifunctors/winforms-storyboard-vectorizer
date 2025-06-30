7c7246aa1bd1fd5d518434bc5625f650928acc9d
Matthew Gracey <m.gracey1409@gmail.com>
2025-06-30 13:19:42 +0100

    Initial setup

---
+++ WinformsStoryboardVectoriser/StoryboardSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace WinformsStoryboardVectorizer;
+
+public class StoryboardSerializer {
+    private Dictionary<Type, Func<Control, string>> converters = [];
+
+    public void Register<T>(Func<T, string> converter) where T : Control {
+        converters.Add(typeof(T), converter);
+    }
+
+    public string Serialize(Form form) {
+
+    }
+}
